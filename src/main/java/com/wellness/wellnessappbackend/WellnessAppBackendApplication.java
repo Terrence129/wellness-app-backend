f16f26033625a9a6fe235a93041849eab65fa06b
chenyaqi <terrence.yaqi.chen@u.nus.edu>
2026-06-25 17:26:41 +0800

    Init

---
+++ src/main/java/com/wellness/wellnessappbackend/WellnessAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wellness.wellnessappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellnessAppBackendApplication.class, args);
+    }
+
+}
